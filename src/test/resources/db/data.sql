c6d8ff5757b2b7f90c8f794247727e18ed477900
yareach8345 <mine8345@naver.com>
2026-02-13 14:05:49 +0900

    fix - h2의 대소문자 구분에 맞추기 위해 sql 파일 수정

--- src/test/resources/db/data.sql
+++ src/test/resources/db/data.sql
@@ -1,23 +1,23 @@
-drop table if exists vote;
-drop table if exists vote_record;
+drop table if exists "vote";
+drop table if exists "vote_record";
 
-create table if not exists vote (
+create table if not exists "vote" (
     id varchar(36) default random_uuid() primary key,
-    is_open boolean not null default false,
-    started_at datetime default null,
-    ended_at datetime default null,
-    created_at datetime not null default current_timestamp
+    "is_open" boolean not null default false,
+    "started_at" datetime default null,
+    "ended_at" datetime default null,
+    "created_at" datetime not null default current_timestamp
 );
 
-create table if not exists vote_record (
+create table if not exists "vote_record" (
     id int auto_increment primary key,
-    vote_id varchar(36) not null,
-    user_id varchar(255) not null,
-    item varchar(255) not null,
-    voted_at datetime not null default current_timestamp,
+    "vote_id" varchar(36) not null,
+    "user_id" varchar(255) not null,
+    "item" varchar(255) not null,
+    "voted_at" datetime not null default current_timestamp,
 
-    foreign key(vote_id) references vote(id) on delete cascade
+    foreign key("vote_id") references "vote"(id) on delete cascade
 );
 
-insert into vote(id) values (default);
-insert into vote(id) values (default);
+insert into "vote"(id) values (default);
+insert into "vote"(id) values (default);
